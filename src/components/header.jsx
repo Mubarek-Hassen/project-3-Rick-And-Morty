c0974df70c25750cf4681eb95da1ead5b9d67f15
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2022-05-15 22:59:52 -0400

    CSS attached to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom'
+import '../styles/header.css'
 
 const Header = ()=>{
     return (
         <section>
-        <h1>RICK AND MORTY</h1>
-        <img src="https://vish213-rick-and-morty.netlify.app/static/media/logo.eab63707.png" alt="" />
+        <h1>Welcome to the</h1>
+        <img src="https://vish213-rick-and-morty.netlify.app/static/media/logo.eab63707.png" alt="Rick And Morty" className='header-img' />
             <nav>
                 <Link to={'/'}>Home </Link>
                 <Link to={'/'}>About </Link>
                 <Link to={'/'}>More </Link>
-
             </nav>
             </section>
     )
